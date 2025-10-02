697d895cc039a518b55bffa3461858e27e7f43bb
GoOcto <paul@goocto.com>
2025-10-01 23:47:58 -0600

    tweak the trainer

--- go.sh
+++ go.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-#    --dataset_path "./training_data/canon/anya*" \
-
 accelerate launch train_lora.py \
     --model_name "meta-llama/Meta-Llama-3-8B-Instruct" \
-    --dataset_path "training_data/_cast/*.jsonl" \
-    --output_dir "weights/llama_cast" \
-    --epochs 5 \
-    --learning_rate 2e-4 \
+    --dataset_path "training_data/late/*.jsonl" \
+    --epochs 10 \
+    --learning_rate 5e-5 \
     --lr_scheduler_type "cosine" \
+    --batch_size 1 \
     --lora_rank 8 \
-    --batch_size 1
+    --output_dir "weights/lora-10-01" \
--- train_lora.py
+++ train_lora.py
@@ -104,7 +104,7 @@ def main(args):
         output_dir=args.output_dir,
         num_train_epochs=args.epochs,
         per_device_train_batch_size=args.batch_size,
-        gradient_accumulation_steps=2,
+        gradient_accumulation_steps=1,
         optim="paged_adamw_32bit",
         learning_rate=args.learning_rate,
         weight_decay=0.001,
@@ -115,9 +115,9 @@ def main(args):
         warmup_ratio=0.03,
         group_by_length=True,
         lr_scheduler_type=args.lr_scheduler_type,
-        logging_steps=25,
-        save_steps=50,
-        save_total_limit=2,
+        logging_steps=10,
+        save_steps=10,
+        save_total_limit=10,
         report_to="tensorboard",
     )
     print("Training arguments configured.")
