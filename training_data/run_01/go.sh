fb27665f0cfe9c1d1d554a61427fca159301a3b5
GoOcto <paul@goocto.com>
2025-10-02 19:38:59 -0600

    change epochs

--- training_data/run_01/go.sh
+++ training_data/run_01/go.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# try to target ~500 steps
+# here we have 88 training samples
+# with batch size 1 and 6 epochs, that's 528 steps
+
 accelerate launch ../../train_lora.py \
     --model_name "meta-llama/Meta-Llama-3-8B-Instruct" \
     --dataset_path "*.jsonl" \
-    --epochs 8 \
+    --epochs 6 \
     --learning_rate 1e-5 \
     --lr_scheduler_type "cosine" \
     --batch_size 1 \
